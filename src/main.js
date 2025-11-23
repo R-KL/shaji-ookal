9c5bffb7b9688af71cdcb87679697304aab7290e
R-KL <richardkliju@gmail.com>
2025-11-23 23:19:02 +0530

    - Finally Added some animations on the website   using anime.js - Also edited the html to make it look a little bit   better
    To-Do:
    
    - [x] Add animations to the website using anime.js
    - [x] Rewrite entire CSS using Tailwind CSS(mostly)

--- src/main.js
+++ src/main.js
@@ -1,11 +1,53 @@
 import '/src/style.css'
 import '@fontsource-variable/inter'
 import roasts from '/src/roasts.json'
+import { spring } from 'animejs/easings'
+import { stagger } from 'animejs/utils'
+import { animate } from 'animejs/animation'
 import Alpine from 'alpinejs'
 window.Alpine = Alpine
 
-
 const random = arr => arr[Math.floor(Math.random() * arr.length)];
+
+Alpine.data('anime', () => ({
+    easeType: spring({ bounce: -0.5, duration: 368 }),
+
+    appear(el, from) {
+        if (from === undefined || from === 'top') {
+            animate(el, {
+                y: ['-100vh', 0],
+                duration: 1250,
+                delay: stagger(65, { from: 'center' }),
+                ease: this.easeType ,
+            });
+        }
+        else if (from === 'bottom') {
+            animate(el, {
+                y: ['100vh', 0],
+                duration: 1250,
+                delay: stagger(65, { from: 'center' }),
+                ease: this.easeType,
+            });
+        }
+        else if (from === 'left') {
+            animate(el, {
+                x: ['-100vw', 0],
+                duration: 1250,
+                delay: stagger(65, { from: 'center' }),
+                ease: this.easeType,
+            });
+        }
+        else if (from === 'right') {
+            animate(el, {
+                x: ['100vw', 0],
+                duration: 1250,
+                delay: stagger(65, { from: 'center' }),
+                ease: this.easeType,
+            });
+        }
+    },
+}));
+
 Alpine.data('ookal', () => ({
     samsungRoasts: roasts.samsung,
     tvmRoasts: roasts.tvm,
@@ -20,15 +62,15 @@ Alpine.data('ookal', () => ({
     show(text) {
         this.displayText = text;
     },
-    roastAny() {
+    roastAny(el) {
         const pick = Math.random();
         if (pick < 0.4) { this.sCount++; this.show(random(this.samsungRoasts)); }
         else if (pick < 0.8) { this.tCount++; this.show(random(this.tvmRoasts)); }
         else { this.sCount++; this.tCount++; this.show(random(this.combos)); }
-        this.pop();
+        this.pop(el);
     },
-    roastSamsung() { this.sCount++; this.show(random(this.samsungRoasts)); this.pop(); },
-    roastTvm() { this.tCount++; this.show(random(this.tvmRoasts)); this.pop(); },
+    roastSamsung(el) { this.sCount++; this.show(random(this.samsungRoasts)); this.pop(el); },
+    roastTvm(el) { this.tCount++; this.show(random(this.tvmRoasts)); this.pop(el); },
     async copyRoast() {
         try {
             const copyText = 'Neeraj is Someone that can be defined as: ' + this.displayText;
@@ -59,14 +101,17 @@ Alpine.data('ookal', () => ({
             };
         }
     },
-    pop() {
-        this.$refs.display.animate([
-            { transform: 'scale(1)', filter: 'brightness(1)' },
-            { transform: 'scale(1.02)', filter: 'brightness(1.08)' },
-            { transform: 'scale(1)', filter: 'brightness(1)' }
-        ], { duration: 160, easing: 'ease-out' });
+    pop(el) {
+        if (!el) return;
+        animate(el, {
+            transform: ['scale(1)', 'scale(1.2)', 'scale(1)'],
+            filter: ['brightness(1)', 'brightness(1.3)', 'brightness(1)'],
+            duration: 160,
+            easing: 'inOutSine',
+        });
     }
 
 }));
+
 Alpine.start()
 
